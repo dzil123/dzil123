ca265275ce7b54c1d65172496e9b0a2b513a4db6
Daniel Z <dzil123@users.noreply.github.com>
2016-01-10 21:54:22 -0800

    added testing for template

--- web_app.py
+++ web_app.py
@@ -8,14 +8,14 @@ import beaker.middleware
 def index():
     return site_map() # just return function here, dont do any coding
 
+@asdf('Welcome to my Website')
 @route('/site_map', method='ANY')
 @route('/sitemap', method='ANY')
 @route('/map', method='ANY')
 def site_map():
     site = [['Cookie Counter', '/cookie'], ['See the Source Code of this website', '/source']]
 
-    html = '<head><title>Daniel\'s Website</title><link rel="icon" href="/favicon.ico"></head><h1>Welcome to my Hand-Coded Website</h1>' # header
-    html += "<ul>" # begin list
+    html = "<ul>" # begin list
 
     for x in site:
         html += '<li><a href="%s">%s</a></li>' % (x[1], x[0])
